e20b74b9c19c46636a3867ba3832c38c5c555baf
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2020-02-09 02:25:42 +0200

    Added option to use hostpath

--- deploy.sh
+++ deploy.sh
@@ -25,6 +25,8 @@ done
 GH_PROFILE=""
 LXD_BR_NAME="lxdbr0"
 CLOBBER=0
+WITH_HOSTPATH=0
+WITH_ISCSI=0
 
 throw() {
     echo $1
@@ -34,10 +36,12 @@ throw() {
 usage() {
     echo "$0 flags
     
---github-user   The github username from which we fetch the public key
---lxd-br-name   LXD bridge name to use for VMs. Defaults to lxdbr0.
---admin-user    The admin user with full sudo access. Defaults to ubuntu.
---clobber       Use this option to overwrite any existing settings with those in this script."
+--github-user [YOUR_USERNAME]   The github username from which we fetch the public key
+--lxd-br-name [lxdbr0]          LXD bridge name to use for VMs. Defaults to lxdbr0.
+--admin-user [ubuntu]           The admin user with full sudo access. Defaults to ubuntu.
+--clobber                       Use this option to overwrite any existing settings with those in this script.
+--with-hostpath [1|0]           Install HostPath CSI. Node will use large disk profile.
+--with-iscsi    [1|0]           Install iSCSI external provisioner. Adds an extra VM."
 }
 
 while [ $# -gt 0 ]
@@ -53,6 +57,12 @@ do
         CLOBBER=1;;
     --admin-user)
         ADMIN_USER=$2
+        shift;;
+    --with-hostpath)
+        WITH_HOSTPATH=$2
+        shift;;
+    --with-iscsi)
+        WITH_ISCSI=$2
         shift;;
     *)
         usage
@@ -168,40 +178,69 @@ create_iscsi_storage_class() {
     lxc exec $VM_MASTER -- /bin/bash -c 'sudo -i -u '$ADMIN_USER' -- kubectl create -f /home/'$ADMIN_USER'/iscsi/iscsi-provisioner-class.yaml'
 }
 
+setup_hostpath() {
+    lxc exec $VM_MASTER -- /bin/bash -c 'sudo -i -u '$ADMIN_USER' -- /bin/bash -c "git clone https://github.com/kubernetes-csi/external-snapshotter.git" /home/'$ADMIN_USER'/external-snapshotter'
+    lxc exec $VM_MASTER -- /bin/bash -c 'sudo -i -u '$ADMIN_USER' -- /bin/bash -c "kubectl create -f /home/'$ADMIN_USER'/external-snapshotter/config/crd/"'
+    lxc exec $VM_MASTER -- /bin/bash -c 'sudo -i -u '$ADMIN_USER' -- /bin/bash -c "kubectl create -f /home/'$ADMIN_USER'/external-snapshotter/deploy/kubernetes/snapshot-controller/"'
+
+    lxc exec $VM_MASTER -- /bin/bash -c 'sudo -i -u '$ADMIN_USER' -- /bin/bash -c "git clone https://github.com/kubernetes-csi/csi-driver-host-path.git /home/'$ADMIN_USER'/csi-driver-host-path"'
+    lxc exec $VM_MASTER -- /bin/bash -c 'sudo -i -u '$ADMIN_USER' -- /bin/bash -c "cd /home/'$ADMIN_USER'/csi-driver-host-path/deploy/kubernetes-latest/ && ./deploy-hostpath.sh"'
+    lxc exec $VM_MASTER -- /bin/bash -c 'sudo -i -u '$ADMIN_USER' -- /bin/bash -c "kubectl create -f /home/'$ADMIN_USER'/csi-driver-host-path/examples/csi-storageclass.yaml"'
+}
+
 create_machines() {
+    NODE_DS="vm-ds-large"
+    if [ $WITH_HOSTPATH -eq 1 ]
+    then
+        NODE_DS="vm-ds-xlarge"
+    fi
     lxc launch ubuntu:18.04 --vm --profile=$VM_PROFILE_NAME --profile=vm-medium --profile=vm-ds-small $VM_MASTER
-    lxc launch ubuntu:18.04 --vm --profile=$VM_PROFILE_NAME --profile=vm-large --profile=vm-ds-large $VM_NODE
-    lxc launch ubuntu:18.04 --vm --profile=$VM_PROFILE_NAME --profile=vm-small --profile=vm-ds-xlarge $VM_ISCSI
+    lxc launch ubuntu:18.04 --vm --profile=$VM_PROFILE_NAME --profile=vm-large --profile=$NODE_DS $VM_NODE
+    if [ $WITH_ISCSI -eq 1 ]
+    then
+        lxc launch ubuntu:18.04 --vm --profile=$VM_PROFILE_NAME --profile=vm-small --profile=vm-ds-xlarge $VM_ISCSI
+        (wait_for_agent $VM_ISCSI) &
+    fi
 
     (wait_for_agent $VM_MASTER) &
     (wait_for_agent $VM_NODE) &
-    (wait_for_agent $VM_ISCSI) &
 
     wait || throw "Failed to spawn vms"
 
+    LXC_INFO=`lxc list $VM_MASTER --format yaml`
+    MASTER_SRV_IP=`python3 -c 'import yaml; import sys; data=yaml.safe_load(sys.argv[1]); net=data[0]["state"]["network"]; print([j["address"] for i in net for j in net[i]["addresses"] if i != "lo" and j["family"] == "inet"][0])' "$LXC_INFO"`
+
     SETUP_SCRIPT_PATH="$HERE/setup_k8s.sh"
-    SETUP_NFS="$HERE/setup_k8s_iscsi.sh"
     SETUP_KUBEVIRT_SCRIPT_PATH="$HERE/setup_kubevirt.sh"
     lxc file push $SETUP_SCRIPT_PATH $VM_MASTER/setup_k8s.sh
     lxc file push $SETUP_KUBEVIRT_SCRIPT_PATH $VM_MASTER/setup_kubevirt.sh
-    lxc file push $SETUP_NFS $VM_ISCSI/setup_k8s_iscsi.sh
+
+    TARGET_PASSWD=`uuidgen`
+    ISCSI_PID=""
+    if [ $WITH_ISCSI -eq 1 ]
+    then
+        SETUP_ISCSI="$HERE/setup_k8s_iscsi.sh"
+        lxc file push $SETUP_ISCSI $VM_ISCSI/setup_k8s_iscsi.sh
+        (lxc exec $VM_ISCSI -- /bin/bash -c '/setup_k8s_iscsi.sh --password '$TARGET_PASSWD'') &
+        ISCSI_PID=$!
+    fi
+
     lxc exec $VM_MASTER -- /bin/bash -c 'chmod +x /setup_*'
     lxc file push $SETUP_SCRIPT_PATH $VM_NODE/setup_k8s.sh
     lxc exec $VM_NODE -- /bin/bash -c 'chmod +x /setup_*'
 
-    (lxc exec $VM_MASTER -- /bin/bash -c '/setup_k8s.sh --role master --admin-user '$ADMIN_USER'') &
+    (lxc exec $VM_MASTER -- /bin/bash -c '/setup_k8s.sh --master-ip '$MASTER_SRV_IP' --role master --with-iscsi '$WITH_ISCSI' --with-hostpath '$WITH_HOSTPATH' --admin-user '$ADMIN_USER'') &
     MASTER_PID=$!
 
-    (lxc exec $VM_NODE -- /bin/bash -c '/setup_k8s.sh --admin-user '$ADMIN_USER'') &
+    (lxc exec $VM_NODE -- /bin/bash -c '/setup_k8s.sh --with-iscsi '$WITH_ISCSI' --with-hostpath '$WITH_HOSTPATH' --admin-user '$ADMIN_USER'') &
     NODE_PID=$!
-
-    TARGET_PASSWD=`uuidgen`
-    (lxc exec $VM_ISCSI -- /bin/bash -c '/setup_k8s_iscsi.sh --password '$TARGET_PASSWD'') &
-    NFS_PID=$!
 
     wait $NODE_PID || throw "failed to set up k8s on $VM_NODE"
     wait $MASTER_PID || throw "failed to set up k8s on $VM_MASTER"
-    wait $NFS_PID || throw "failed to set up k8s NFS on $VM_ISCSI"
+    if [ ! -z $ISCSI_PID ]
+    then
+        wait $ISCSI_PID || throw "failed to set up k8s NFS on $VM_ISCSI"
+    fi
 
     # join node to master
     JOIN_CMD=`lxc exec $VM_MASTER -- /bin/bash -c 'kubeadm token create --print-join-command 2>&1 | tail -n1'`
@@ -209,7 +248,15 @@ create_machines() {
     sleep 60
     lxc exec $VM_MASTER -- /bin/bash -c '/setup_kubevirt.sh --admin-user '$ADMIN_USER''
 
-    create_iscsi_storage_class $TARGET_PASSWD
+    if [ $WITH_ISCSI -eq 1 ]
+    then
+        create_iscsi_storage_class $TARGET_PASSWD
+    fi
+
+    if [ $WITH_HOSTPATH -eq 1 ]
+    then
+        setup_hostpath
+    fi
 }
 
 create_profiles
--- setup_k8s.sh
+++ setup_k8s.sh
@@ -5,6 +5,9 @@ set -e
 POD_CIDR="10.244.0.0/16"
 ROLE="node"
 ADMIN_USER="ubuntu"
+WITH_ISCSI=0
+WITH_HOSTPATH=0
+MASTER_IP="127.0.0.1"
 
 while [ $# -gt 0 ]
 do
@@ -18,6 +21,15 @@ do
     --admin-user)
         ADMIN_USER=${2}
         shift;;
+    --with-iscsi)
+        WITH_ISCSI=${2}
+        shift;;
+    --with-hostpath)
+        WITH_HOSTPATH=${2}
+        shift;;
+    --master-ip)
+        MASTER_IP=${2}
+        shift;;
     esac
     shift
 done
@@ -27,11 +39,20 @@ throw() {
 	exit 1
 }
 
-getent passwd $ADMIN_USER || throw "admin user $ADMIN_USER does not exist"
+[ $WITH_ISCSI -eq 0 ] && [ $WITH_HOSTPATH -eq 0 ] && throw "at least one CSI must be enabled"
+getent passwd $ADMIN_USER 2>&1 > /dev/null || throw "admin user $ADMIN_USER does not exist"
 
 create_docker_group() {
 	sudo addgroup --system docker || true
     sudo adduser $ADMIN_USER docker || true
+}
+
+setup_iscsi() {
+    sudo apt-get install -y -q open-iscsi
+    echo "InitiatorName=iqn.1993-08.org.debian:01:$HOSTNAME" > /etc/iscsi/initiatorname.iscsi
+    sudo systemctl enable iscsid
+    sudo systemctl stop iscsid || true
+    sudo systemctl start iscsid
 }
 
 setup_k8s() {
@@ -40,19 +61,16 @@ setup_k8s() {
 	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     #sudo add-apt-repository -y ppa:gluster/glusterfs-7
 	#sudo apt install -y docker-ce apt-transport-https glusterfs-client
-	sudo apt install -y docker-ce apt-transport-https
+	sudo apt install -y -q docker-ce apt-transport-https
 	echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-	sudo apt-get update
-	sudo apt install -y kubeadm  kubelet kubernetes-cni jq open-iscsi
-
-    echo "InitiatorName=iqn.1993-08.org.debian:01:$HOSTNAME" > /etc/iscsi/initiatorname.iscsi
-    sudo systemctl enable iscsid
-    sudo systemctl stop iscsid || true
-    sudo systemctl start iscsid
+	sudo apt-get update -q
+	sudo apt install -y -q kubeadm  kubelet kubernetes-cni jq git
 }
 
 setup_master() {
-	sudo kubeadm init --pod-network-cidr=$POD_CIDR
+    DEFAULTS=`kubeadm config print init-defaults`
+    python3 -c 'import yaml;import sys; data=list(yaml.safe_load_all(sys.argv[1])); doc=[i for i in data if i.get("controllerManager", None) is not None][0]; doc["controllerManager"] = {"extraArgs": {"enable-hostpath-provisioner": "true"}};doc["networking"]["podSubnet"] = "'$POD_CIDR'"; doc2 = [i for i in data if i.get("localAPIEndpoint", None) is not None][0]; doc2["localAPIEndpoint"]["advertiseAddress"] = "'$MASTER_IP'" ;print(yaml.dump_all(data))' "$DEFAULTS" > /tmp/kubeadm.cfg
+	sudo kubeadm init --config=/tmp/kubeadm.cfg
 
     USER_HOME=`eval echo ~$ADMIN_USER`
 	mkdir -p $USER_HOME/.kube
@@ -68,11 +86,16 @@ setup_pod_network() {
 	sudo -i -u $ADMIN_USER -- kubectl apply -f /tmp/kube-flannel.yml
 }
 
+echo "Running create_docker_group on $HOSTNAME"
 create_docker_group
+echo "Running setup_k8s on $HOSTNAME"
 setup_k8s
+[ $WITH_ISCSI -eq 1 ] && setup_iscsi
 
 if [ $ROLE == "master" ]
 then
+    echo "Running setup_master on $HOSTNAME"
     setup_master
+    echo "Running setup_pod_network on $HOSTNAME"
     setup_pod_network
 fi
